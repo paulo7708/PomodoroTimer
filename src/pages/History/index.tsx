60c56872bf20aee1d8314acacc7008b576828538
Paulo7708 <dawnbreakerph@gmail.com>
2023-05-16 20:04:50 -0300

    History

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,65 @@
+import { HistoryContainer, Historylist, Status } from "./styles";
+
+
+export function History(){
+  return(
+    <HistoryContainer>
+      <h1>History</h1>
+
+      <Historylist>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Enterrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Historylist>
+    </HistoryContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,23 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em:</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1"/>
+            <option value="Project 2"/>
+            <option value="Project 3"/>
+            <option value="Banana"/>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             placeholder="00"
             id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
