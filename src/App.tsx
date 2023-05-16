a8ee08fc74f7880ee6c77403081825fceac60ea9
Paulo7708 <dawnbreakerph@gmail.com>
2023-05-16 13:11:13 -0300

    Feat: Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import "./App.css";
-import { Button } from "./components/button/Button";
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from './styles/themes/default'
+import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-    
   );
 }
 
